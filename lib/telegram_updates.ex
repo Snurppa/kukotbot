4ccb24d83e10782d167369c4610fa1784503ef6c
Snurppa <snurppa@gmail.com>
2017-01-17 18:55:41 +0200

    do json stuff in http.json

--- lib/telegram_updates.ex
+++ lib/telegram_updates.ex
@@ -1,11 +1,18 @@
 defmodule Telegram.Updates do
+  @updates_path "/getUpdates"
 
-  def get_updates, do: HTTP.JSON.get!(Telegram.bot_url <> "/getUpdates").body
+  def get_updates do
+    HTTP.JSON.get!(Telegram.bot_url <> @updates_path).body
+  end
 
   def get_updates(update_id) do
-    body = Poison.encode!(%{"offset": update_id})
-    {:ok, resp} = HTTP.JSON.post(Telegram.bot_url <> "/getUpdates", body, [{"Content-type", "application/json"}])
-    resp.body
+    payload = %{"offset": update_id}
+    case HTTP.JSON.post(Telegram.bot_url <> @updates_path, payload) do
+      {:ok, response} ->
+        response.body
+      {:error, response} ->
+        raise "Telegram getUpdates failure: #{response}"
+    end
   end
 
 end
