47cc32dc8fcbc7681a36f9d9cdb81e9c260e5b07
Joni Hämäläinen <Joni.Hamalainen@solita.fi>
2017-08-29 11:46:25 +0300

    try to implement supervisor similar as culttt.com

--- lib/kukotbot.ex
+++ lib/kukotbot.ex
@@ -2,14 +2,6 @@ defmodule Kukotbot do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
-    children = [
-      #worker(Kukotbot.Web, []),
-      worker(Telegram.Updates, [])
-    ]
-
-    opts = [strategy: :one_for_one, name: Kukotbot.Supervisor, max_restarts: 5, max_seconds: 10]
-    Supervisor.start_link(children, opts)
+    Kukotbot.Supervisor.start_link
   end
 end
--- lib/kukotbot_supervisor.ex
+++ lib/kukotbot_supervisor.ex
@@ -1,3 +1,17 @@
 defmodule Kukotbot.Supervisor do
+  use Supervisor
 
+  def start_link do
+    Supervisor.start_link(__MODULE__, [])
+  end
+
+  def init(_) do
+    children = [
+      #worker(Kukotbot.Web, []),
+      worker(Telegram.Updates, [])
+    ]
+
+    opts = [strategy: :one_for_one, max_restarts: 5, max_seconds: 10]
+    supervise(children, opts)
+  end
 end
