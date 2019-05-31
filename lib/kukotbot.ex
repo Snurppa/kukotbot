b1fec5e84a1a3ef980ed8b288936cda70dc81315
Snurppa <snurppa@gmail.com>
2019-05-31 16:14:22 +0300

    increase restart tolerance

--- lib/kukotbot.ex
+++ lib/kukotbot.ex
@@ -6,6 +6,8 @@ defmodule Kukotbot do
       {Kukotbot.State, name: BotState},
       {Telegram.Updates, [BotState]}
     ]
-    Supervisor.start_link(children, [strategy: :one_for_one])
+    Supervisor.start_link(children, [strategy: :one_for_one,
+                                     max_restarts: 10,
+                                     max_seconds: 5])
   end
 end
