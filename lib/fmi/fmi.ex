c991556e25e7a0a7f265fdb32afc3b28af25cde1
Joni Hämäläinen <snurppa@gmail.com>
2022-11-05 00:59:01 +0200

    hirlam deprecated, use ecmwf forecast

--- lib/fmi/fmi.ex
+++ lib/fmi/fmi.ex
@@ -5,29 +5,40 @@ defmodule FMI do
   require Logger
 
   @fmi_get_feature_url Application.get_env(:kukotbot, :fmi_host) <> "/wfs?request=getFeature"
-  @fmi_hirlam_point_timevalue "fmi::forecast::hirlam::surface::point::timevaluepair"
+  @ecmwf_point "ecmwf::forecast::surface::point::timevaluepair"
 
-  def surface_point_url_for_place(place) do
-    @fmi_get_feature_url <> "&storedquery_id=" <> @fmi_hirlam_point_timevalue <> "&place=" <> place
+  def query_feature(stored_query, params_str) do
+    HTTPoison.get(@fmi_get_feature_url <> "&storedquery_id=" <> stored_query <> "&" <> params_str)
   end
 
-  def parse_temps(xmlstr) do
+  def get_xml_temps(xml_data) do
+    Exml.get(xml_data, "/wfs:FeatureCollection/wfs:member/omso:PointTimeSeriesObservation[.//wml2:MeasurementTimeseries[@gml:id='mts-1-1-Temperature']]//wml2:MeasurementTimeseries//wml2:MeasurementTVP")
+  end
+
+  def parse_xml(xmlstr) do
     xmlstr
     |> String.replace("\n","")
     |> Exml.parse([space: :normalize])
-    |> Exml.get("/wfs:FeatureCollection/wfs:member/omso:PointTimeSeriesObservation[.//wml2:MeasurementTimeseries[@gml:id='mts-1-1-Temperature']]//wml2:MeasurementTimeseries//wml2:MeasurementTVP")
+  end
+
+  def get_temps(xmlstr) do
+    parse_xml(xmlstr)
+    |> get_xml_temps
+  end
+
+  def sanitized_temps(temps) do
+    temps
+    |> Enum.map(fn measurement -> Enum.filter(measurement, fn row_item -> byte_size(String.trim(row_item)) > 0 end) end)
   end
 
   def search_weather(place) when byte_size(place) > 0  do
     Logger.info fn -> "Searching weather for #{place}" end
-    url = surface_point_url_for_place(place)
-    case HTTPoison.get(url) do
+    case query_feature(@ecmwf_point, "place=" <> place) do
       {:ok, %{status_code: 200, body: body}} ->
         Logger.info fn -> "200 Weather received for #{place}" end
-        temps = parse_temps(body)
-        sanitized_temps = temps
-        |> Enum.map(fn measurement -> Enum.filter(measurement, fn row_item -> byte_size(String.trim(row_item)) > 0 end) end)
-        sanitized_temps
+        body
+        |> get_temps
+        |> sanitized_temps
       {:ok, %{status_code: 400}} ->
         Logger.warn fn -> "400 Weather error for #{place}" end
       {:error, %{reason: reason}} ->
